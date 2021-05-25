a73c3342de01b541922c52d3ac1defccb9ee490f
wkrl <kwxrl@yandex.com>
2021-05-25 15:28:45 +0200

    Add annotation parsing to extractor

--- Makefile
+++ Makefile
@@ -4,19 +4,25 @@ VERSION=1.0.0
 max_length = 100
 output_file = 'wikipedia-$(max_length)chars.json'
 
-all: fetch extract parse merge
+kb: fetch extract parse_without_annotations merge
+
+kb_annotated: fetch extract parse_with_annotations merge
 
 fetch:
 	pip3 install -r requirements.txt
 	python3 download_data.py
 
 extract:
-	python3 -m wikiextractor.WikiExtractor *.xml.* --output ./extracted --json --quiet
+	python3 -m wikiextractor.WikiExtractor *.xml.* --output ./extracted --links --json --quiet
 	
-parse: 
+parse_with_annotations: 
+	mkdir -p parsed
+	python3 wikiextractor_to_texoo.py --annotate
+
+parse_without_annotations:
 	mkdir -p parsed
 	python3 wikiextractor_to_texoo.py --max_length $(max_length)
-	
+
 merge: 
 	python3 merge_datasets.py --output ${output_file}
 
--- wikiextractor_to_texoo.py
+++ wikiextractor_to_texoo.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
-from texoopy import Dataset, Document
+from texoopy import Dataset, Document, NamedEntityAnnotation
 from typing import Union
 import glob
 import json
 import os
 from tqdm import tqdm
 import argparse
+from bs4 import BeautifulSoup
 
 
 class AbtractKBConverter(ABC):
@@ -19,31 +20,64 @@ class AbtractKBConverter(ABC):
         pass
 
 class WikipediaWikiExtractor(AbtractKBConverter):
-    def __init__(self, path_to_wikiextractor_output: str, name: str = 'Wikipedia', text_limit: Union[None, int] = None):
+    def __init__(self, path_to_wikiextractor_output: str, name: str = 'Wikipedia', annotate: bool = False, text_limit: Union[None, int] = None):
         """
         Parses the output of WikiExtractor, e.g. python3 -m wikiextractor.WikiExtractor ... --json ... enwiki-20200201-pages-articles.xml
         :param path_to_wikiextractor_output: Path to the result WikiExtractor
         :param name: Name of the dataset
+        :param annotate: If references should be parsed as annotations
         :param text_limit: Cut description text after X characters to reduce memory consumption
         """
         self.path_to_wikiextractor_output: str = path_to_wikiextractor_output
         self.name: str = name
+        self.annotate = annotate
         self.text_limit = text_limit
         self.parse_wikiextractor_files()
-    
+
+    def extract_annotations(self, soup):
+        annotations = []
+        text = soup.get_text()
+        for a in soup.findAll('a'):
+            annotation = NamedEntityAnnotation(
+                begin=text.index(a.text), # assuming that the first occurence of the word is the reference
+                length=len(a.text),
+                text=a.text,
+                source='GOLD',
+                confidence=1.0,
+                refId=f'https://en.wikipedia.org/wiki/{a["href"]}',
+                candidates=[]
+            )
+            annotations.append(annotation)
+        return annotations
+
+    def generate_document(self, wikipedia_page):
+        text = ' '.join(wikipedia_page['text'].split('\n\n')[1:]) # remove the title from the text
+        soup = BeautifulSoup(text, 'lxml')
+        text = soup.get_text()
+
+        if self.text_limit is not None:
+            text = text[:self.text_limit]
+
+        annotations = self.extract_annotations(soup) if self.annotate == True else []
+        doc = Document(
+            id=wikipedia_page['id'],
+            title=wikipedia_page['title'],
+            text=text,
+            length=len(text),
+            annotations=annotations
+        )
+        return doc
+
     def parse_wikiextractor_files(self):
         self.kb = Dataset(language='en', name=self.name)
         pattern = os.path.join(self.path_to_wikiextractor_output, '**/*')
         filepaths = [filepath for filepath in glob.glob(pattern, recursive=True) if os.path.isfile(filepath)]
-        
-        for file in filepaths:            
+
+        for file in filepaths:
             with open(file, 'r') as f:
                 for line in f:
                     wikipedia_page = json.loads(line)
-                    text = ' '.join(wikipedia_page['text'].split('\n\n')[1:])  # remove the title from the text
-                    if self.text_limit is not None:
-                        text = text[:self.text_limit]
-                    doc = Document(id=wikipedia_page['id'], title=wikipedia_page['title'], text=text, length=len(text))
+                    doc = self.generate_document(wikipedia_page)
                     self.kb.documents.append(doc)
 
     def get_kb(self) -> Dataset:
@@ -51,17 +85,18 @@ class WikipediaWikiExtractor(AbtractKBConverter):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser();
-    parser.add_argument('--max_length', nargs=1, type=int, action='store', help='Max article length')    
+    parser.add_argument('--max_length', nargs=1, type=int, action='store', default=[None], help='Max article length')
+    parser.add_argument('--annotate', action='store_true', default=False, help='Annotate article references')
     args = parser.parse_args()
-    
-    text_limit = args.max_length[0]
+    text_limit, annotate = args.max_length[0], args.annotate
+
     subfolders = [subfolder for subfolder in glob.glob('./extracted/*', recursive=False)]
 
     if not os.path.exists(f'./parsed/'):
         os.makedirs(f'./parsed/')
 
     for subfolder in tqdm(subfolders):
-        kb = WikipediaWikiExtractor(subfolder, text_limit=text_limit).get_kb()           
+        kb = WikipediaWikiExtractor(subfolder, annotate=annotate, text_limit=text_limit).get_kb()
         if not os.path.exists(f'./parsed/{subfolder[-2:]}'):
             os.makedirs(f'./parsed/{subfolder[-2:]}')
         with open(f'./parsed/{subfolder[-2:]}/wikipedia_{text_limit}chars.json', 'w') as f:
